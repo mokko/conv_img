43db4bf434f8dd1dd61b811f5802cd7fc6bd1e67
Maurice Mengel <mauricemengel@gmail.com>
2024-07-26 22:18:04 +0200

    improve argparse help messages

--- src/conv_img/__init__.py
+++ src/conv_img/__init__.py
@@ -7,41 +7,41 @@ import argparse
 
 def conv_img():
     parser = argparse.ArgumentParser(
-        description="convert tifs to pngs while preserving paths intelligently"
+        description="Yet another CLI util to convert images"
     )
 
     parser.add_argument(
         "-a",
         "--act",
         action="store_true",
-        help="specify a limit to stop at",
+        help="signal to carry out conversion instead of just showing what would have been",
     )
 
     parser.add_argument(
-        "-l", "--limit", type=int, help="specify a limit to stop at", default=-1
+        "-l", "--limit", type=int, help="number of images after which script breaks off, defaults to -1 (no limit)", default=-1
     )
 
     parser.add_argument(
-        "-m", "--max_size", type=int, help="max size of pixel", default=6000
+        "-m", "--max_size", type=int, help="max size of pixel, defaults to 6000 pixel for the longest size", default=6000
     )
 
     parser.add_argument(
         "-f",
         "--filemask",
-        help="specify a filemask, defaults to '**/*.tif'",
+        help="the filemask that identifies which images will be converted, defaults to '**/*.tif'",
         default="**/*.tif",
     )
 
     parser.add_argument(
         "-t",
         "--target_suffix",
-        help="specify target image format, defaults to '.jpg'",
+        help="target image format, defaults to '.jpg'",
         default=".jpg",
     )
 
     parser.add_argument(
         "dest",
-        help="specify a destination directory to write to",
+        help="destination directory to write to",
     )
     args = parser.parse_args()
 
