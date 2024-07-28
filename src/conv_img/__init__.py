ec029774da78a7272c29dd8ac808656f44483095
Maurice Mengel <mauricemengel@gmail.com>
2024-07-28 12:02:02 +0200

    add verbose flag, refactor __init__, bump version

--- src/conv_img/__init__.py
+++ src/conv_img/__init__.py
@@ -1,64 +1,9 @@
 """Yet another CLI util to convert images"""
 
-__version__ = "0.0.1"  # inital version
+__version__ = "0.0.2"  # move arguments from __init__ to script
 
-import argparse
-from conv_img.conv_img import main
+from conv_img.conv_img import arg as cv_arg
 
 
 def conv_img():
-    parser = argparse.ArgumentParser(
-        description="Yet another CLI util to convert images"
-    )
-
-    parser.add_argument(
-        "-a",
-        "--act",
-        action="store_true",
-        help="signal to carry out conversion instead of just showing what would have been",
-    )
-
-    parser.add_argument(
-        "-f",
-        "--filemask",
-        help="the filemask that identifies which images will be converted, defaults to '**/*.tif'",
-        default="**/*.tif",
-    )
-
-    parser.add_argument(
-        "-l",
-        "--limit",
-        type=int,
-        help="number of images after which script breaks off, defaults to -1 (no limit)",
-        default=-1,
-    )
-
-    parser.add_argument(
-        "-m",
-        "--max_size",
-        type=int,
-        help="max size of pixel, defaults to 6000 pixel for the longest size",
-        default=6000,
-    )
-
-    parser.add_argument(
-        "-t",
-        "--target_suffix",
-        help="target image format, defaults to '.jpg'",
-        default=".jpg",
-    )
-
-    parser.add_argument(
-        "dest",
-        help="destination directory to write to",
-    )
-    args = parser.parse_args()
-
-    main(
-        act=args.act,
-        dest=args.dest,
-        filemask=args.filemask,
-        limit=args.limit,
-        max_size=args.max_size,
-        target_suffix=args.target_suffix,
-    )
+    cv_arg()
--- src/conv_img/conv_img.py
+++ src/conv_img/conv_img.py
@@ -1,57 +1,134 @@
 """
-conv_img 
+conv_img
 Yet another CLI utility to batch convert image files
 """
 
-from PIL import Image
+from PIL import Image as Img
+from PIL.Image import Image
 from pathlib import Path
+import argparse
+
+_verbose = False
+
+
+def arg():
+    parser = argparse.ArgumentParser(
+        description="Yet another CLI util to convert images"
+    )
+
+    parser.add_argument(
+        "-a",
+        "--act",
+        action="store_true",
+        help="carry out conversion instead of just showing what would have been",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--filemask",
+        help="the filemask that identifies which images will be converted, defaults to '**/*.tif'",
+        default="**/*.tif",
+    )
+
+    parser.add_argument(
+        "-l",
+        "--limit",
+        type=int,
+        help="number of images after which script breaks off, defaults to -1 (no limit)",
+        default=-1,
+    )
+
+    parser.add_argument(
+        "-m",
+        "--max_size",
+        type=int,
+        help="max size of pixel, defaults to 6000 pixel for the longest size",
+        default=6000,
+    )
+
+    parser.add_argument(
+        "-t",
+        "--target_suffix",
+        help="target image format, defaults to '.jpg'",
+        default=".jpg",
+    )
+
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        help="for more verbose output",
+        action="store_true",
+    )
+
+    parser.add_argument(
+        "dest_dir",
+        help="destination directory to write to",
+    )
+    args = parser.parse_args()
+
+    main(
+        act=args.act,
+        dest_dir=args.dest_dir,
+        filemask=args.filemask,
+        limit=args.limit,
+        max_size=args.max_size,
+        target_suffix=args.target_suffix,
+    )
 
 
 def convert_img(src: Path, dest: Path, max_size: int) -> None:
-    """Convert the image at src and save it at dest, resizing to max_size if original
-    image exceeds max size."""
-    #print("   converting")
-    img = Image.open(src)
+    """
+    Convert the image at src and save it at dest, resizing to max_size if original image
+    exceeds max size.
+    """
+    # print("   converting")
+    img = Img.open(src)
     img = resize(img, max_size=max_size)
     img.save(dest, optimize=True)
 
 
-def convert_path(*, p: Path, dest: str, target_suffix: str) -> Path:
+def convert_path(*, p: Path, dest_dir: str, target_suffix: str) -> Path:
     """
-    from source path, create destination path (not making any directories)
+    from source path p create destination path (not making any directories)
+
+    If dest_dir is %, we save the new image file in-place, i.e. we return a path in the
+    same folder as the original.
     """
-    if dest == "%": # in place
+    if dest_dir == "%":
         return p.with_suffix(target_suffix)
-    name2 = p.with_suffix(target_suffix).name
-    return Path(dest) / p.parent / name2
+    return Path(dest_dir) / p.parent / p.with_suffix(target_suffix).name
 
 
 def main(
     *,
     act: bool,
-    dest: str,
+    dest_dir: str,
     filemask: str,
     limit: int,
     target_suffix: str,
     max_size: int,
+    verbose: bool = False,
 ) -> None:
-    if limit:
-        print(f"limit: {limit}")
-    if act:
-        print("act: True")
-    print(f"filemask: {filemask}")
-    if not target_suffix.startswith ("."):
-        raise SyntaxError(f"The target suffix has to start with .: '{target_suffix}'")
-    print(f"target suffix: {target_suffix}'")
-    print(f"max_size: {max_size}")
+    if verbose:
+        global _verbose
+        _verbose = True
+
+    show_state(
+        act=act,
+        dest_dir=dest_dir,
+        filemask=filemask,
+        limit=limit,
+        max_size=max_size,
+        target_suffix=target_suffix,
+    )
 
     for c, p in enumerate(Path(".").glob(filemask), start=1):
         p2 = convert_path(
-            p=p, dest=dest, target_suffix=target_suffix
+            p=p, dest_dir=dest_dir, target_suffix=target_suffix
         )  # haven't created new dir yet
         print(f"{c}:{p.name} -> {p2}")
         if p2.exists():
-            print(f"   exists already")
+            v(f"   exists already")
         else:
             if act:
                 p2.parent.mkdir(parents=True, exist_ok=True)
@@ -62,6 +139,10 @@ def main(
 
 
 def resize(img, *, max_size: int) -> Image:
+    """
+    Receive an image object, resize it to max_size if longest side exceeds that
+    number of pixels and return the image object.
+    """
     width, height = img.size
     if width > max_size or height > max_size:
         if width > height:
@@ -69,10 +150,38 @@ def resize(img, *, max_size: int) -> Image:
         else:  # height > width or both equal
             factor = max_size / height
         new_size = (int(width * factor), int(height * factor))
-        print(f"   resize {factor:.0%} {new_size}")
-        return img.resize(new_size, Image.LANCZOS)
+        v(f"   resize {factor:.0%} {new_size}")
+        return img.resize(new_size, Img.LANCZOS)
     return img
 
 
+def show_state(
+    *,
+    act: bool,
+    dest_dir: str,
+    filemask: str,
+    limit: int,
+    max_size: int,
+    target_suffix: str,
+) -> None:
+    """
+    Print state to STDOUT if global flag _verbose is set.
+    """
+    if limit:
+        v(f"limit: {limit}")
+    if act:
+        v("act: True")
+    v(f"filemask: {filemask}")
+    if not target_suffix.startswith("."):
+        raise SyntaxError(f"The target suffix has to start with .: '{target_suffix}'")
+    v(f"target suffix: {target_suffix}'")
+    v(f"max_size: {max_size}")
+
+
+def v(msg: str) -> None:
+    if _verbose:
+        print(msg)
+
+
 if __name__ == "__main__":
-    pass
+    arg()
